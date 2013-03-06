4f1ad885df65fb5ab33323524dba027b6021388d
Jon Wong <j@jnwng.com>
2013-03-05 17:10:08 -0800

    Getting image path working on virus info

--- app/viperdb/templatetags/get_image_path.py
+++ app/viperdb/templatetags/get_image_path.py
@@ -2,10 +2,18 @@ from django import template
 
 register = template.Library()
 
-def image_path(virus, image_type, image, filetype="jpg"):
- 	virus_name = virus.name
-	path = "images/%s/%s-%s.%s" % (image_type, virus_name, image, filetype)
+def image_path(virus, path_info ):
 
-	return path
+    path_info = path_info.split(',')
+
+    image_type = path_info[0]
+    image = path_info[1]
+    filetype = "jpg"# path_info[2]
+
+    virus_name = virus.entry_id
+
+    path = "http://localhost:80/images/%s/%s-%s.%s" % (image_type, virus_name, image, filetype)
+
+    return path
 
 register.filter('image_path', image_path)
