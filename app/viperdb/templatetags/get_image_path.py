a481fdbcb2b14b1e7fcad1c31c4b4e253ef7f8df
Jon Wong <j@jnwng.com>
2013-08-09 14:15:19 -0700

    catching loose files

--- app/viperdb/templatetags/get_image_path.py
+++ app/viperdb/templatetags/get_image_path.py
@@ -4,10 +4,8 @@ register = template.Library()
 
 def image_path(entry_id, image_type, image_subtype):
 
-    # path = "http://localhost:80/images/%s/%s-%s" % \
-    #        (image_type, entry_id, image_subtype)
-    path = "/viperdb3-analysis/images/%s/%s/%s" % \
-            (image_type, entry_id, image_subtype)
+    path = "http://localhost:80/images/%s/%s-%s" % \
+           (image_type, entry_id, image_subtype)
     return path
 
 register.simple_tag(image_path)
